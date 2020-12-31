f248273b263ce07b711ecaba5da7a8a8825e16d8
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2020-12-31 13:04:18 +0300

    Add Admin Panel Menu Hook
    Add Admin Panel Menu

---
+++ wordpress-admin-menu.php
@@ -0,0 +1,22 @@
+/**
+ * Register a custom menu page.
+ */
+function wpdocs_register_my_custom_menu_page(){
+    add_menu_page( 
+        __( 'Custom Menu Title', 'textdomain' ),
+        'custom menu',
+        'manage_options',
+        'custompage',
+        'my_custom_menu_page',
+        plugins_url( 'myplugin/images/icon.png' ),
+        6
+    ); 
+}
+add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
+ 
+/**
+ * Display a custom menu page
+ */
+function my_custom_menu_page(){
+    esc_html_e( 'Admin Page Test', 'textdomain' );  
+}
