3dc7f11e06402a313ebe10288895e766f42642be
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2021-01-03 18:48:53 +0300

    WooCommerce Before Customer Login

---
+++ woocommerce-before-customer-login.php
@@ -0,0 +1,15 @@
+add_action('woocommerce_before_customer_login_form', 'custom_login_text');
+
+ function custom_login_text()
+    {
+        if (!is_user_logged_in()) {
+
+            // The displayed (output)
+            if (get_page_uri() !== 'kayit-ol') {
+                echo '<p style="border:font-size: large;">' . __("<b>Üye Değil misiniz?</b> <b><a href='https://test.yachtier.com/kayit-ol/'>Hemen Üye Olun<a/></b>", "woocommerce") . '</p>';
+
+            } else {
+                echo '';
+            }
+        }
+    }
