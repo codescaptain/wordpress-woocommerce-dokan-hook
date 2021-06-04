c242fa279ef50c30224e14c178aa6caec51e2820
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2021-06-04 21:51:41 +0300

    Update Status Text
    Update Status Text

--- woocommerce-custom-order-status.php
+++ woocommerce-custom-order-status.php
@@ -24,7 +24,7 @@
            // 'exclude_from_search' => false,
            // 'show_in_admin_all_list' => true,
            // 'show_in_admin_status_list' => true,
-           // 'label_count' => _n_noop('Kargolandı <span class="count">(%s)</span>', 'İptal Talep Edildi <span class="count">(%s)</span>')
+           // 'label_count' => _n_noop('İptal Talep Edildi <span class="count">(%s)</span>', 'İptal Talep Edildi <span class="count">(%s)</span>')
        // ));
         register_post_status('wc-refunded-progress', array(
             'label' => __('İade Talep Edildi', 'woocommerce'),
@@ -32,9 +32,9 @@
             'exclude_from_search' => false,
             'show_in_admin_all_list' => true,
             'show_in_admin_status_list' => true,
-            'label_count' => _n_noop('Kargolandı <span class="count">(%s)</span>', 'İade Talep Edildi <span class="count">(%s)</span>')
+            'label_count' => _n_noop('İade Talep Edildi <span class="count">(%s)</span>', 'İade Talep Edildi <span class="count">(%s)</span>')
         ));
     }
 
-add_action('init', array(new KKEP_Dokan(), 'register_WC_shipped_status'));
-add_filter('wc_order_statuses', array(new KKEP_Dokan(), 'add_WC_shipped_status'), 10, 3);
+add_action('init', 'register_WC_shipped_status');
+add_filter('wc_order_statuses',  'add_WC_shipped_status', 10, 3);
