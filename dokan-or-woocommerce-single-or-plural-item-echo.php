bfb35047cb3804d77be27a9d9224cd5c6bb5aefa
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2021-06-08 13:10:04 +0300

    dokan or woocommerce single or-plural item echo

---
+++ dokan-or-woocommerce-single-or-plural-item-echo.php
@@ -0,0 +1,5 @@
+ <?php 
+ /**
+ * @param int $item_count
+ */
+ echo wp_kses_post(sprintf(_n('%s for %s item', '%s for %s items', $item_count, 'dokan-lite'), $order->get_formatted_order_total(), $item_count)); 
